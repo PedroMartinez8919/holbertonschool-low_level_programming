7203bc21823806612122113fadc74c4a08e18080
PedroMartinez8919 <2390@holbertonschool.com>
2021-06-29 21:46:49 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - The function return the length of a string.  
+ *
+ * @s:is the pointer.
+ * Return: slong
+ */
+int _strlen(char *s)
+{
+	int slong = 0;
+
+	while (*s != '\0')
+	{
+		slong++;
+		s++;
+	}
+	return (slong);
+}
