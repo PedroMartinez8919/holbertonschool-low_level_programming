8016889500d446b22650edbc1d6c916c9515ae8f
PedroMartinez8919 <2390@holbertonschool.com>
2021-07-21 12:05:01 +0000

    Putchar a Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main ()
+{
+  char alph = 'a';
+  char ALPH = 'A';
+
+  while(alph <= 'z')
+    { putchar(alph);
+      alph++;
+    }
+  while (ALPH <= 'Z')
+    { putchar (ALPH);
+      ALPH++;
+    }
+      putchar ('\n');
+      return (0);
+    }
