910ac6a943bbe56b27fae25550c640b553490d46
PedroMartinez8919 <2390@holbertonschool.com>
2021-07-22 13:31:12 +0000

    Printing Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - Entry Point.
+ *
+ * Return : Always 0 (Success).
+ */
+int main(void)
+{
+char alphabet = 'a'; 
+while (alphabet <= 'z')
+{ 
+_putchar (alphabet);
+alphabet++;
+}
+_putchar ('\n');
+return (0);
+}
