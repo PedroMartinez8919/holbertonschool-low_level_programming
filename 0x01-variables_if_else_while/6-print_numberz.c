aa9cc1cdeb5e05b80e1f358b9f4bb079361075bc
PedroMartinez8919 <2390@holbertonschool.com>
2021-07-22 03:23:08 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+int numbs = '0';
+while (numbs <= '9')
+{ putchar (numbs);
+numbs++;
+}
+putchar ('\n');
+return (0);
+}
