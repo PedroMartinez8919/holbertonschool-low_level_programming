19a28d41b15b240e9637a424fa6040f924ff439d
PedroMartinez8919 <2390@holbertonschool.com>
2021-07-22 03:42:05 +0000

    Mirror print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char Betalpha = 'z';
+while (Betalpha >= 'a')
+{ putchar (Betalpha);
+Betalpha--;
+}
+putchar ('\n');
+return (0);
+}
